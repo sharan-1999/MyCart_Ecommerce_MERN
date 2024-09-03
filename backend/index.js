fdbca5b41a1ad1130da2b3449c62d2d6978660ce
sharan-1999 <sunny.avsharan@gmail.com>
2024-09-03 21:45:51 +0530

    offline changes

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT || 5000
 dotenv.config();
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+))
 
 mongoose
     .connect(process.env.MONGO_URL, {
